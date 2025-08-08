608c78e8c258b25ddc6f0c71a6cf4449b87920ac
gbolaga-daramola <gbolagadaramola765@gmail.com>
2025-08-08 10:42:33 +0100

    Complete work - deploy to vercel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='card'>
+        <img src='./avatar-jessica.jpeg' alt=''/>
+        <div className='content'>
+          <h1>Jessica Randall</h1>
+          <h3>London, United Kingdom</h3>
+        </div>
+        <p className='spaced-text'>"Front-end developer and avid reader."</p>
+
+        <div className='links'>
+          <a href='https://github.com'>GitHub</a>
+          <a href='https://www.frontendmentor.io/'>Frontend Mentor</a>
+          <a href='https://www.linkedin.com'>Linkedin</a>
+          <a href='https://x.com'>Twitter</a>
+          <a href='https://www.instagram.com/'>Instagram</a>
+        </div>
+      </div>
+    </main>
   );
 }
 
